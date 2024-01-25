d8122c714bdce6f795684d526ee00e71904ac885
Jack <905182279@qq.com>
2024-01-25 19:46:57 +0800

    feat: wallet page

--- src/welcome/index.tsx
+++ src/welcome/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import Wallet from './Wallet';
 import Welcome from './Welcome';
 
 import '../tailwind.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Welcome />
+    {/* <Welcome /> */}
+    <Wallet />
   </React.StrictMode>
 );
