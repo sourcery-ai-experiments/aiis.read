e771d73d3114db8f1442e6c5f73b68d24ac467c9
Jack <905182279@qq.com>
2024-02-04 16:32:38 +0800

    feat: review style and replace logo

--- src/welcome/index.tsx
+++ src/welcome/index.tsx
@@ -7,7 +7,7 @@ import '../tailwind.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* <Profile /> */}
-    <Wallet />
+    <Profile />
+    {/* <Wallet /> */}
   </React.StrictMode>
 );
