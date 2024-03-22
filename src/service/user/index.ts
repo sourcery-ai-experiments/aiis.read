f661865d0b03a021c2262580cb19fd7e65311a1b
Dendi <pingqian1991@gmail.com>
2024-03-22 15:43:58 +0800

    fix: change accounts

--- src/service/user/index.ts
+++ src/service/user/index.ts
@@ -2,7 +2,7 @@ import { useRequest } from 'ahooks';
 
 import useUserStore from '../../store/useUserStore';
 import http, { ResultData } from '../request';
-
+import { XFANS_USERINFO } from '../../constants';
 const useUserInfo = () => {
   const result = useRequest<ResultData<UserInfo>, unknown[]>(() => http.get('/api/user/me'), {
     manual: true,
@@ -10,6 +10,7 @@ const useUserInfo = () => {
       useUserStore.setState({
         userInfo: response.data,
       });
+      localStorage.setItem(XFANS_USERINFO, JSON.stringify(response.data));
     },
   });
 
